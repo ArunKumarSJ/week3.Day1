ae9dfc37d39a51d9dc0d950fd475005d9afa4d30
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-08-21 22:19:13 +0530

    Day 5 Assignment

---
+++ Bankinfo.java
@@ -0,0 +1,14 @@
+package week3.Day1;
+
+public class Bankinfo {
+	public void saving() {
+		System.out.println("This is Saving Account :");
+	}
+	public void fixed() {
+		System.out.println("This is Fixed Depoist Account"); 
+	}
+	public void deposit() {
+		System.out.println("Rs.45,000 is deposited in Atm Deposit Machine ");
+	}
+
+}
