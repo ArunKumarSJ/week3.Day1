53e6cbaecbdcd457141b1756fd247dd7d3d3435a
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-08-21 22:24:30 +0530

    College info

---
+++ College.java
@@ -0,0 +1,14 @@
+package week3.Day1;
+
+public class College {
+	public void collegename() {
+		System.out.println("The college Name is KLN");
+	}
+	public void collegecode() {
+		System.out.println("The college Code is 25484");
+	}
+	public void collegerank(){
+		System.out.println("The rank of college is 2nd ");
+	}
+
+}
