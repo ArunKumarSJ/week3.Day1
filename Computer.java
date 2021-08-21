f2a83b7b1420118205c93b9d3f8050a8caa5438f
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-08-21 22:21:30 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,16 @@
+package week3.Day1;
+
+public class Computer {
+	public void computermodel1() {
+		System.out.println("The First Computer Model is HP Slimline");
+
+	}
+	public void computerModel2() {
+		System.out.println("The Second Computer Modeel is Dell");
+	}
+	public void computerModel3() {
+		System.out.println("The Third Computer Model is Apple iMac ");
+		
+	}
+
+}
